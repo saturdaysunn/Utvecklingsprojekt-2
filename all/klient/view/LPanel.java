fdc5f9495c846a82b5415d290279a3f512894dec
saturdaysunn <tiffanydzdr@gmail.com>
2024-02-24 12:01:47 +0100

    updated button and chat view
    added "view chat" and "upload image" buttons as well as chat view. Added some methods without functionality for when sending messages is possible.

--- all/klient/view/LPanel.java
+++ all/klient/view/LPanel.java
@@ -23,6 +23,7 @@ public class LPanel extends JPanel{
     private MainFrame mainFrame;
     private JList<String> leftPanelList;
     private JButton addToContactsButton;
+    private JButton viewChatButton;
     private List<String> selectedUsers;
     private ArrayList<String> currentUserContacts = new ArrayList<>();
     private HashMap<String, ArrayList<String>> userContacts = new HashMap<>();
@@ -56,15 +57,20 @@ public class LPanel extends JPanel{
         JScrollPane scrollPane = new JScrollPane(leftPanelList); //should be scrollable when many users online
         add(scrollPane, BorderLayout.CENTER);
 
-        populateLPanel(readFromFile("all/files/users.txt"));
+        populateLPanel(readFromFile("all/files/users.txt")); //TODO: fix so updated from server, not file
 
-        JPanel addContactPanel = new JPanel(new FlowLayout());
+        JPanel buttonPanel = new JPanel(new FlowLayout());
         addToContactsButton = new JButton("Add to Contacts");
-        addContactPanel.add(addToContactsButton); //add button to panel
-        addContactPanel.setPreferredSize(new Dimension(20, 90));
+        viewChatButton = new JButton("View Chat");
+        buttonPanel.add(addToContactsButton); //add button to panel
+        buttonPanel.add(viewChatButton); //add button to panel
+        buttonPanel.setPreferredSize(new Dimension(20, 100));
+
+
         addToContactsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //TODO: what if several have been selected?
                 Object selectedObject = leftPanelList.getSelectedValue();
 
                 if (selectedObject != null) {
@@ -89,7 +95,23 @@ public class LPanel extends JPanel{
                 }
             }
         });
-        add(addContactPanel, BorderLayout.SOUTH); //add to south of panel
+
+        viewChatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<String> selectedObjects = leftPanelList.getSelectedValuesList();
+                if (selectedObjects.size() == 1) { //check so only one user has been selected
+                    String selectedObject = selectedObjects.get(0);
+                    if (selectedObject != null) {
+                        mainFrame.getMainPanel().getcPanel().viewChat(selectedObject);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(buttonPanel, "Please select only one user to view chat.", "Selection Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        add(buttonPanel, BorderLayout.SOUTH); //add to south of panel
     }
 
     /**
@@ -97,9 +119,7 @@ public class LPanel extends JPanel{
      * @param contactsList
      */
     protected void populateLPanel(List<String> contactsList){
-
         String[] contactsArray = contactsList.toArray(String[]::new);
-
         leftPanelList.setListData(contactsArray);
     }
 
@@ -118,7 +138,6 @@ public class LPanel extends JPanel{
     }
 
     public static void writeHashMapToFile(HashMap<String, ArrayList<String>> hashMap, String filename) {
-
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
             for (Map.Entry<String, ArrayList<String>> entry : hashMap.entrySet()) {
                 String key = entry.getKey();
