094f1db4949806eefae4ba4958570c25219cf6e3
Mihail Spasovski <124456259+suizmuiz@users.noreply.github.com>
2024-03-07 21:23:49 +0100

    unlimited panels

--- all/Main.java
+++ all/Main.java
@@ -5,7 +5,5 @@ import all.klient.boundary.LoginPanel;
 public class Main {
     public static void main(String[] args) {
         new LoginPanel();
-        new LoginPanel();
-        new LoginPanel();
     }
 }
